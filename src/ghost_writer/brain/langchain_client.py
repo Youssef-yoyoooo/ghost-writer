f4635712472e99bcbad2d2e1d236e3ce851a4a1e
Youssef-yoyoooo <yousse.bassem1@msa.edu.eg>
2026-05-07 17:35:26 +0300

    fix: update langchain imports for compatibility with v1.x

--- src/ghost_writer/brain/langchain_client.py
+++ src/ghost_writer/brain/langchain_client.py
@@ -1,5 +1,5 @@
 from langchain_community.llms import Ollama
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 from .prompts import QA_ENGINEER_PROMPT
 
 class BrainClient:
